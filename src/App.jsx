fbcc673391701b8139903a0ea771f968813274ef
bjorn <bjornverbak2006@gmail.com>
2025-09-23 14:19:19 +0200

    update iframe link 2

--- src/App.jsx
+++ src/App.jsx
@@ -185,7 +185,7 @@ const Model = memo(({ isRotating, ...props }) => {
             occlude="blending"
           >
             <iframe
-              src="https://3d-monitor-meiv-ls645h5ig-bjorns-projects-5466d641.vercel.app/"
+              src="https://3d-monitor-meiv.vercel.app/"
               title="OS"
             >
             </iframe>
